bf6bad8c15a4c899cd111ce0b2b79dd7167a19dd
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2021-04-22 17:57:15 +0100

    Added another API Key

--- src/ExampleClass.cs
+++ src/ExampleClass.cs
@@ -3,6 +3,8 @@ namespace SomeNamespace {
   public class SomeClass {
 
     // This is a dummy/test value
-    public string ApiKey => "sfsfk204ksfoksoksokfosfk";
+    public string ApiKey1 => "sfsfk204ksfoksoksokfosfokokk";
+
+    public string ApiKey2 => "sfsfk204ksfoksoksokfosfokokk";
   }
 }
